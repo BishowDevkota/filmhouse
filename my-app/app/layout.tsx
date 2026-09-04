8e4d14dcb036631095ee07d52a5daec680ebc9a8
BishowDevkota <bishow8848@gmail.com>
2026-09-04 13:57:53 +0545

    scripts added

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -38,6 +38,20 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             TMDB. Built as a learning project.
           </p>
         </footer>
+
+        {/* Ad tags (zones 11723367 / 11723369 / 11723371). afterInteractive
+            keeps them off the critical path. */}
+        <Script
+          src="https://5gvci.com/act/files/tag.min.js?z=11723367"
+          data-cfasync="false"
+          strategy="afterInteractive"
+        />
+        <Script id="ad-zone-11723369" strategy="afterInteractive">
+          {`(function(s){s.dataset.zone='11723369',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`}
+        </Script>
+        <Script id="ad-zone-11723371" strategy="afterInteractive">
+          {`(function(s){s.dataset.zone='11723371',s.src='https://n6wxm.com/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`}
+        </Script>
       </body>
     </html>
   );
